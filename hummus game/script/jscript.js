9cdc5c7cfecd169ed08178c82458fc8ebba60e0f
liorweitz <liorweitz@gmail.com>
2016-06-20 16:18:10 +0300

    finished-on our way to cheat!!!!

--- hummus game/script/jscript.js
+++ hummus game/script/jscript.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
 //from here
     var ingredientsCounter={parsley:1, hummus:1, ful:1, egg:1};
+    var onThePlate = {hummus:0, ful:0, egg:0, parsely:0};
 //gives the serving utensils data about what they can scoop
     $(".spatula").attr('data-can-scoop', 'hummus');
     $(".tongs").attr('data-can-scoop', 'egg');
@@ -21,6 +22,15 @@ $(document).ready(function () {
         utensils.removeClass('selected');
         selectedUtensil.addClass('selected');
     });
+    var egg=Math.floor((Math.random()*4)+1);
+    var ful=Math.floor((Math.random()*4)+1);
+    var hummus=Math.floor((Math.random()*4)+1);
+    var orderObject={egg:egg, hummus:hummus, ful:ful};
+
+    $('.first-line').text('egg: '+egg);
+    $('.second-line').text('Hummus: '+hummus);
+    $('.third-line').text('Ful: '+ful);
+
     //when trying to scoop
     var ingredients=$('div.ingredients div');
     ingredients.on('click', function () {
@@ -29,18 +39,25 @@ $(document).ready(function () {
         if (askedIngredient===$('.selected').attr('data-can-scoop') && ingredientsCounter[askedIngredient]<3){
             ingredientsCounter[askedIngredient]++;
             $('.'+askedIngredient+'-tray').css('background-image','url("./images/tray-'+askedIngredient+'-'+ingredientsCounter[askedIngredient]+'.png")');
+            onThePlate[askedIngredient]++;
+
         }
         else if(ingredientsCounter[askedIngredient]>=3){
             $('.'+askedIngredient+'-tray').css('background-image','url("./images/tray-1.png")')
         }
 
-        else if(askedIngredient!==selectedUtensil.attr('data-can-scoop')){
-            selectedUtensil.addClass('wrong-tool');
+        else if(askedIngredient!==$('.selected').attr('data-can-scoop')){
+            $('.selected').addClass('wrong-tool');
             setTimeout(function () {
-                selectedUtensil.removeClass('wrong-tool');
+                $('.selected').removeClass('wrong-tool');
             },200);
         }
-
+        setTimeout(function () {
+            $('#client').css('background-image','url("./images/ninja-character-2.png")');
+        },4000);
+        setTimeout(function () {
+            failed();
+        },8000);
     });
 
     var failed= function () {
@@ -51,48 +68,51 @@ $(document).ready(function () {
         $('body').addClass('failed');
     };
     //creates an order
-    var createFirstOrder=function () {
-        var egg=Math.floor((Math.random()*4)+1);
-        var ful=Math.floor((Math.random()*4)+1);
-        var hummus=Math.floor((Math.random()*4)+1);
-        var orderObject={egg:egg, hummus:hummus, ful:ful};
-        $('.first-line').text('egg: '+egg);
-        $('.second-line').text('Hummus: '+hummus);
-        $('.third-line').text('Ful: '+ful);
-        return orderObject;
-    };
+    // var createFirstOrder=function () {
+    //     var egg=Math.floor((Math.random()*4)+1);
+    //     var ful=Math.floor((Math.random()*4)+1);
+    //     var hummus=Math.floor((Math.random()*4)+1);
+    //     var orderObject={egg:egg, hummus:hummus, ful:ful};
+    //     var beginOrderObject={egg:0, hummus:0, ful:0};
+    //     $('.first-line').text('egg: '+egg);
+    //     $('.second-line').text('Hummus: '+hummus);
+    //     $('.third-line').text('Ful: '+ful);
+    //
+    //
+    // };
 
-    var orderIngredientsCounter=createFirstOrder();
-    var createOrder = function (orderCounter) {
-      var egg = orderCounter.egg + Math.floor((Math.random()*2) + 1);
-      var ful = orderCounter.ful + Math.floor((Math.random()*3) + 1);
-      var hummus = orderCounter.hummus + Math.floor((Math.random()*2) + 1);
-      var orderObj = {egg:egg, hummus:hummus, ful:ful};
-        $('.first-line').text('egg: '+egg);
-        $('.second-line').text('Hummus: '+hummus);
-        $('.third-line').text('Ful: '+ful);
-        return orderObj;
-    }
-    var onThePlate = {hummus:0, ful:0, egg:0, parsely:0};
-    //dealing with the order
-    var dealing=function (orderIngredientsCounter, timeStepCounter, timeStep) {
-      var origin = orderIngredientsCounter;
-
-      if(origin.egg <= onThePlate.egg &&
-         origin.ful <= onThePlate.ful &&
-         origin.hummus <= onThePlate.hummus) {
-           setTimeout(function() {dealing(createOrder(origin), 0, timeStep - 1000);}, 1000);
-         }
-      if(timeStepCounter >= 3) {
-        failed();
-      }
-      else {
-        $("#client").css("background-image", "url('../images/ninja-character-"+ (timeStepCounter+1) + ".png')");
-        setTimeout(function(){dealing(origin, timeStepCounter + 1, timeStep);}, timeStep);
-      }
-    }
-
-    dealing(orderIngredientsCounter, 0, 12000);
+    // var orderIngredientsCounter=createFirstOrder();
+    // var createOrder = function (orderCounter) {
+    //   var egg = orderCounter.egg + Math.floor((Math.random()*2) + 1);
+    //   var ful = orderCounter.ful + Math.floor((Math.random()*3) + 1);
+    //   var hummus = orderCounter.hummus + Math.floor((Math.random()*2) + 1);
+    //   var orderObj = {egg:egg, hummus:hummus, ful:ful};
+    //     $('.first-line').text('egg: '+egg);
+    //     $('.second-line').text('Hummus: '+hummus);
+    //     $('.third-line').text('Ful: '+ful);
+    //     return orderObj;
+    // }
+    //
+    // //dealing with the order
+    // var dealing=function (orderIngredientsCounter, timeStepCounter, timeStep) {
+    //   var origin = orderIngredientsCounter;
+    //
+    //     if(origin.egg <= onThePlate.egg &&
+    //         origin.ful <= onThePlate.ful &&
+    //         origin.hummus <= onThePlate.hummus) {
+    //         onThePlate = {hummus:0, ful:0, egg:0, parsely:0};
+    //         setTimeout(function() {dealing(createOrder(origin), 0, timeStep - 1000);}, 1000);
+    //     }
+    //   if(timeStepCounter >= 3) {
+    //     failed();
+    //   }
+    //   // else {
+    //   //   $("#client").css("background-image", "url('../images/ninja-character-"+ (timeStepCounter+1) + ".png')");
+    //   //   setTimeout(function(){dealing(origin, timeStepCounter + 1, timeStep);}, timeStep);
+    //   // }
+    // }
+    //
+    // dealing(orderIngredientsCounter, 0, 12000);
 
 
 
